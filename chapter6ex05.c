9cd965b70b48c6481f6316520871c8d9aa303e3e
HectorPat <83776645+HectorPat@users.noreply.github.com>
2021-06-23 05:06:59 -0500

    Create chapter6ex05.c

---
+++ chapter6ex05.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+  int año, division, cociente4,cociente100,cociente400;
+
+  printf("ingrese un año\n");
+  scanf("%d", &año);  
+  cociente4=(año%4);
+  cociente100=(año%100);
+  cociente400=(año%400);
+  if (cociente4 == 0){
+    printf("es año biciesto\n");
+  }
+  if(cociente400==0){
+    printf("es año biciesto\n");
+  }
+  else if (cociente100==0){
+    printf("no año biciesto\n");
+  }
+  return 0;
+}
