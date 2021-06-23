a1cc41c3a9909d5208f05980f8775b6b52ffe4cf
HectorPat <83776645+HectorPat@users.noreply.github.com>
2021-06-22 20:57:35 -0500

    Create chapter6ex02.c

---
+++ chapter6ex02.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+  float valor;
+  printf("ingrese una calificación\n");
+  scanf("%f", &valor);
+  if (valor>=101){
+    printf("el valor ingresado es erroneo para a calificar");
+  }
+  if (valor<0){
+    printf("el valor ingresado es erroneo para calificar");
+  return 0;
+  }
+  if (valor>90){
+    printf("tu promedio es A");
+  }
+  else if (valor>80){
+    printf("tu promedio es B");
+  } 
+  else if (valor>70){
+    printf("tu promedio es C");
+  }
+  else if (valor>60){
+    printf("tu promedio es D");
+  }
+  else if (valor<=60){
+    printf("tu promedio es F");
+  }
+
+  return 0;
+}
